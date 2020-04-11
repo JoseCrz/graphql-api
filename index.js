8533135846008c80d03793ee4b7620c8d5d18b2f
Jose Cuevas <josecueram@gmail.com>
2020-04-10 20:54:26 -0500

    created lib directory with graphql schema

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+'use strict'
 const express = require('express')
-const { graphql, buildSchema } = require('graphql')
+const { buildSchema } = require('graphql')
 const gqlMiddleware = require('express-graphql')
+const { readFileSync } = require('fs')
+const { join } = require('path')
 
 const config = require('./config')
 const PORT = config.port
@@ -8,24 +11,20 @@ const PORT = config.port
 const app = express()
 
 // define schema
-const schema = buildSchema(`
-    type Query {
-        hello: String
-    }
-`)
+const schema = buildSchema(readFileSync(join(__dirname, 'lib', 'schema.graphql'), 'utf-8'))
 // define resolver
 const resolvers = {
-    hello: () => {
-        return 'Hello world'
-    }
+  hello: () => {
+    return 'Hello world'
+  }
 }
 
 app.use('/api', gqlMiddleware({
-    schema,
-    rootValue: resolvers,
-    graphiql: true
+  schema,
+  rootValue: resolvers,
+  graphiql: true
 }))
 
 app.listen(PORT, () => {
-    console.log(`[App] Listening on port: ${PORT}`)
+  console.log(`[App] Listening on port: ${PORT}`)
 })
