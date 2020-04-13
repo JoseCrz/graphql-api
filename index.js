2108aff5f2529b26296dee766e5471bdf13f59a2
Jose Cuevas <josecueram@gmail.com>
2020-04-13 14:22:38 -0500

    prepared API for deployment

--- index.js
+++ index.js
@@ -4,23 +4,28 @@ const { makeExecutableSchema } = require('graphql-tools')
 const gqlMiddleware = require('express-graphql')
 const { readFileSync } = require('fs')
 const { join } = require('path')
+const cors = require('cors')
 const resolvers = require('./lib/resolvers')
-// const db = require('./lib/db-mongoose')
+const config = require('./config')
 
+const DEV = config.dev
+const PORT = config.port
+
+// const db = require('./lib/db-mongoose')
 // db.connect()
 
-const config = require('./config')
-const PORT = config.port
 
 const app = express()
 
 const typeDefs = readFileSync(join(__dirname, 'lib', 'schema.graphql'), 'utf-8')
 const schema = makeExecutableSchema({typeDefs, resolvers})
 
+app.use(cors())
+
 app.use('/api', gqlMiddleware({
   schema,
   rootValue: resolvers,
-  graphiql: true
+  graphiql: DEV
 }))
 
 app.listen(PORT, () => {
